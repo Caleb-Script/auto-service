21b4a9012a1b6086e3555b6d9ca308050d87c096
Caleb-Script <caleb-script@outlook.de>
2024-04-16 15:32:51 +0200

    GraphQL config

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,26 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AutoReadService } from './auto/service/auto-read.service';
 import { AutoWriteService } from './auto/service/auto-write.service';
-import { AutoQueryResolver } from './resolver/auto-query.resolver';
 import { graphQlModuleOptions } from './config/ressources/graphql/graphql';
 import { typeOrmModuleOptions } from './config/typeormOptions';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriverConfig } from '@nestjs/apollo';
 import { entities } from './auto/entity/entities';
-import { AutoMutationResolver } from './resolver/auto-Mutation.resolver';
+import { AutoMutationResolver } from './auto/graphql/auto-Mutation.resolver';
+import { AutoQueryResolver } from './auto/graphql/auto-query.resolver';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature(entities),
-    GraphQLModule.forRoot<ApolloDriverConfig>(graphQlModuleOptions),
-    TypeOrmModule.forRoot(typeOrmModuleOptions),
-  ],
-  providers: [
-    AutoReadService,
-    AutoWriteService,
-    AutoQueryResolver,
-    AutoMutationResolver,
-  ],
+    imports: [
+        TypeOrmModule.forFeature(entities),
+        GraphQLModule.forRoot<ApolloDriverConfig>(graphQlModuleOptions),
+        TypeOrmModule.forRoot(typeOrmModuleOptions),
+    ],
+    providers: [
+        AutoReadService,
+        AutoWriteService,
+        AutoQueryResolver,
+        AutoMutationResolver,
+    ],
 })
 export class AppModule {}
--- backend/src/auto/graphql/auto-query.resolver.ts
+++ backend/src/auto/graphql/auto-query.resolver.ts
@@ -1,39 +1,39 @@
 import { Args, Query, Resolver } from '@nestjs/graphql';
-import { Auto } from 'src/auto/entity/auto.entity';
-import { getLogger } from 'src/logger/logger';
-import { AutoReadService } from 'src/auto/service/auto-read.service';
-import { Suchkriterien } from 'src/auto/service/suchkriterien';
+import { AutoReadService } from '../service/auto-read.service';
+import { getLogger } from '../../logger/logger';
+import { Auto } from '../entity/auto.entity';
+import { Suchkriterien } from '../service/suchkriterien';
 
 export interface SuchkriterienInput {
-  readonly suchkriterien: Suchkriterien;
+    readonly suchkriterien: Suchkriterien;
 }
 
 @Resolver()
 export class AutoQueryResolver {
-  readonly #service: AutoReadService;
-  readonly #logger = getLogger(AutoQueryResolver.name);
+    readonly #service: AutoReadService;
+    readonly #logger = getLogger(AutoQueryResolver.name);
 
-  constructor(service: AutoReadService) {
-    this.#service = service;
-  }
+    constructor(service: AutoReadService) {
+        this.#service = service;
+    }
 
-  @Query('auto')
-  async getAuto(@Args('id') id) {
-    this.#logger.debug(`findById: id=${id}`);
+    @Query('auto')
+    async getAuto(@Args('id') id: number) {
+        this.#logger.debug(`findById: id=${id}`);
 
-    const auto: Auto = await this.#service.findById({ id });
+        const auto: Auto = await this.#service.findById({ id });
 
-    this.#logger.debug(`findById: auto=${auto}, titel=${auto.eigentuemer}`);
+        this.#logger.debug(`findById: auto=${auto}, titel=${auto.eigentuemer}`);
 
-    return auto;
-  }
+        return auto;
+    }
 
-  @Query('autos')
-  async getAutos(@Args() input: SuchkriterienInput) {
-    this.#logger.debug(`findById: input=${input}`);
+    @Query('autos')
+    async getAutos(@Args() input: SuchkriterienInput) {
+        this.#logger.debug(`findById: input=${input}`);
 
-    const autos: Auto[] = await this.#service.find(input?.suchkriterien);
-    this.#logger.debug(`find: kunden=${autos}`, autos);
-    return autos;
-  }
+        const autos: Auto[] = await this.#service.find(input?.suchkriterien);
+        this.#logger.debug(`find: kunden=${autos}`, autos);
+        return autos;
+    }
 }
